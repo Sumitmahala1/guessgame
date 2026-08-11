cf76ff08c3da34e48f92bdc8a2630509b002efb8
Sumitmahala1 <sumitforwork11@gmail.com>
2026-08-11 17:21:58 +0530

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,10 @@
+number = 7 # The secret number
+guess = 0
+while guess != number:
+    guess = int(input("Guess the number (1-10): "))
+    if guess == number:
+        print("Correct! You won!")
+    elif guess < number:
+        print("Too low, try again")
+    else:
+        print("Too high, try again")    
